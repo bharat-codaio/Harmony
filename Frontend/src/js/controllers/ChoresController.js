638b37f5bd1a502e3ec8f85a649f8a6457a7f3f8
bharat batra <bbatra@ucsd.edu>
2016-11-03 15:03:41 -0700

    Added main screens for chores

--- Frontend/src/js/controllers/ChoresController.js
+++ Frontend/src/js/controllers/ChoresController.js
@@ -13,6 +13,26 @@
 
 
     function ChoresController(){
+        const vm = this;
+
+        vm.showMyChores = true;//shows mine if true or others if false
+
+        vm.toggleTab = toggleTab;
+        vm.createChore = createChore;
+
+        vm.myChores = {};
+        vm.apartmentChores = {};
+
+
+        function toggleTab(){
+            vm.showMyChores = !vm.showMyChores;
+        }
+
+        function createChore(){
+            toastr.warning("Create new chore cunt");
+        }
+
+
         console.log("Chores Controller Active");
     }
 })();
